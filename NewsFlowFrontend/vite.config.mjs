7bfb973e3db218d2c424b950074e23107faabc8d
spezifisch <spezifisch+git@gmail.com>
2024-08-07 19:25:16 +0200

    builds

--- NewsFlowFrontend/vite.config.mjs
+++ NewsFlowFrontend/vite.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   publicDir: 'public', // This tells Vite where to find public files
   build: {
     outDir: 'dist', // Specify the output directory
