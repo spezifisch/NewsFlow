1e168593bbe100286bddc8e7ec22cfa70999cb99
spezifisch <spezifisch+git@gmail.com>
2024-08-07 08:37:04 +0200

    upgrade

--- NewsFlowFrontend/tailwind.config.js
+++ NewsFlowFrontend/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}'
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
-  }
+    extend: {},
+  },
 }
