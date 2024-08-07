bcf0c3c76a1af9ddaa2aca9e183cc05e574d8aa6
spezifisch <spezifisch+git@gmail.com>
2024-08-07 08:20:43 +0200

    big commit

---
+++ NewsFlowFrontend/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+    root: true,
+    extends: [
+      'eslint:recommended',
+      'plugin:svelte/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      extraFileExtensions: ['.svelte'],
+      project: './tsconfig.json',
+      allowImportingTsExtensions: true
+    },
+    plugins: ['svelte', '@typescript-eslint', 'prettier'],
+    overrides: [
+      {
+        files: ['*.svelte'],
+        parser: 'svelte-eslint-parser',
+        parserOptions: {
+          parser: '@typescript-eslint/parser'
+        }
+      }
+    ],
+    rules: {
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      'svelte3/typescript': () => require('typescript')
+    }
+  }
+  
--- NewsFlowFrontend/vite.config.ts
+++ NewsFlowFrontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import svelteConfig from './svelte.config.js';
+import svelteConfig from './svelte.config.ts';
 
 export default defineConfig({
   plugins: [svelte(svelteConfig)],
