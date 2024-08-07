28703cffcc1f94508e557d765b76b27156efb17d
spezifisch <spezifisch+git@gmail.com>
2024-08-07 11:02:34 +0200

    eslint not happy

--- NewsFlowFrontend/eslint.config.mjs
+++ NewsFlowFrontend/eslint.config.mjs
@@ -2,6 +2,9 @@ import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { sveltePreprocess } from 'svelte-preprocess'
+import typescript from 'typescript'
+import eslintPluginSvelte from 'eslint-plugin-svelte'
 
 // Mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url)
@@ -15,13 +18,13 @@ const compat = new FlatCompat({
 })
 
 export default [
-  // Mimic ESLintRC-style extends
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:svelte/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ),
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'svelte/rule-name': 'error'
+    },
+  },
 
   // Mimic environments
   ...compat.env({
@@ -30,23 +33,26 @@ export default [
     node: true,
   }),
 
-  // Mimic plugins
-  ...compat.plugins('svelte', '@typescript-eslint', 'prettier'),
-
   // Translate an entire config
   ...compat.config({
-    plugins: ['svelte', '@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
       extraFileExtensions: ['.svelte'],
       project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
       allowImportingTsExtensions: true,
+    },
+    settings: {
+      'svelte/preprocess': sveltePreprocess(),
+      'svelte/typescript': () => typescript,
     },
     rules: {
       'prettier/prettier': 'error',
       '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      // Add any other custom rules here
     },
     globals: {
       localStorage: 'readonly',
